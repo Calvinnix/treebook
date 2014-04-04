805e2d4fffd1986dc1117fda0a89d0c02e5aa362
Calvin Nix <calvinnix28@gmail.com>
2014-04-04 09:00:21 -0400

    Added custom routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Treebook::Application.routes.draw do
   devise_for :users
+  
+  #adds /register/ as another way to get to registration
+  
+  devise_scope :user do
+  	get 'register', to: 'devise/registrations#new', as: :register
+  	get 'login', to: 'devise/sessions#new', as: :login
+  	get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+  
+  
   resources :statuses
+  
+  get 'feed', to: 'statuses#index', as: :feed
+  
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
